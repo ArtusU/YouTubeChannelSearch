1d39595c2a99c28de81da4734950a9f2c066c323
Artus U <artusrock@hotmail.com>
2022-04-14 09:58:30 +0100

    Channel Model and Video Model Fixes #1

--- video_tracker/app/views.py
+++ video_tracker/app/views.py
@@ -1,3 +1,10 @@
 from django.shortcuts import render
 
 # Create your views here.
+from .models import Channel, Video, Channel
+
+
+
+def index(request):
+    context = {}
+    return render(request, 'index.html', context)
