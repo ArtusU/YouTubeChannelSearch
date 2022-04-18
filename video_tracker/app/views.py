1366617657c55e2ff85a342adcad75719ba82bc7
Artus U <artusrock@hotmail.com>
2022-04-18 12:44:09 +0100

    Channel Views Fixes #2

--- video_tracker/app/views.py
+++ video_tracker/app/views.py
@@ -1,4 +1,6 @@
+import requests
 from django.shortcuts import render
+from django.conf import settings
 
 # Create your views here.
 from .models import Channel, Video, Channel
@@ -6,5 +8,15 @@ from .models import Channel, Video, Channel
 
 
 def index(request):
-    context = {}
+    channels = Channel.objects.all()
+    context = {'channels': channels}
     return render(request, 'index.html', context)
+
+
+def channel_search(request):
+    query = request.GET.get('q')
+    url = f'https://www.googleapis.com/youtube/v3/search?q={query}&type=channel&part=snippet&key={settings.YOUTUBE_API_KEY}'
+    response = requests.get(url)
+    results = response.json()['items']
+    context = {'results': results}
+    return render(request, 'partials/channel_search_results.html', context=context)
