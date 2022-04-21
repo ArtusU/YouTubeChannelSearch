41d53561e37b9fbd139e1e3640784257de2c086e
Artus U <artusrock@hotmail.com>
2022-04-21 09:35:28 +0100

    Add Channels to Data Base Fixes #5

--- video_tracker/app/views.py
+++ video_tracker/app/views.py
@@ -1,10 +1,11 @@
+from unittest import result
 import requests
 from django.shortcuts import render
 from django.conf import settings
 
 # Create your views here.
 from .models import Channel, Video, Channel
-
+from django.views.decorators.csrf import csrf_exempt
 
 
 def index(request):
@@ -20,3 +21,23 @@ def channel_search(request):
     results = response.json()['items']
     context = {'results': results}
     return render(request, 'partials/channel_search_results.html', context=context)
+
+
+@csrf_exempt
+def add_channel(request, channel_id):
+    url = f'https://www.googleapis.com/youtube/v3/channels?id={channel_id}&part=snippet,contentDetails&key={settings.YOUTUBE_API_KEY}'
+    response = requests.get(url)
+    result = response.json()['items'][0]
+    
+    channel = Channel(
+        name=result['snippet']['title'],
+        playlist_id=result['contentDetails']['relatedPlaylists']['uploads'],
+        thumbnail_url=result['snippet']['thumbnails']['default']['url'],
+        description=result['snippet']['description']
+        )
+    channel.save()
+    
+    channels = Channel.objects.all()
+    context = {'channels': channels}
+    return render(request, 'partials/channels.html', context)
+
